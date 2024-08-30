ff71f40a19d440ab3ea881b38896cd966a8bbfec
Andres Ochoa <andresochoaflorez0@gmail.com>
2024-08-30 02:41:39 -0500

    se corrige error en mensaje de salida de descripcion de marca

--- src/main/java/com/emazon/api/stock/domain/util/brand/Brandconstants.java
+++ src/main/java/com/emazon/api/stock/domain/util/brand/Brandconstants.java
@@ -5,7 +5,7 @@ public class Brandconstants {
     public static final String RETURN_BRAND_CREATED_ERROR_NAME_NULL ="The brand name cant be null" ;
     public static final String RETURN_BRAND_CREATED_ERROR_NAME = "The brand name is between  1 and 50 characters";
     public static final String RETURN_BRAND_CREATED_ERROR_DESCRIPTION_NULL ="The description brand cant be null " ;
-    public static final String RETURN_BRAND_CREATED_ERROR_DESCRIPTION ="The brand description is between  1 and 50 characters" ;
+    public static final String RETURN_BRAND_CREATED_ERROR_DESCRIPTION ="The brand description is between  1 and 120 characters" ;
     public static final String RETURN_ERROR_CREATED_BRAND ="The brand cant created" ;
     public static final String RETURN_ERROR_CREATED_BRAND_SAME_NAME ="this name of brand exist" ;
 }
